f4b897dd53773a3fd1deb3d5660fd5024ff8c801
ValdeMorgen <valdemar.morgen@icloud.com>
2024-02-21 19:08:13 +0100

    TutorialFixes

--- CultGame/Assets/Scripts/TutorialScript.cs
+++ CultGame/Assets/Scripts/TutorialScript.cs
@@ -6,13 +6,14 @@ using TMPro;
 public class TutorialScript : MonoBehaviour
 {
     public int tutorialStage = 0;
+    int tutorialStageCashe = 0;
     public float tutorialTimer = 0;
-    string[] tutorialQuestions = { "Hello, Priest\r\n\r\nI see you have decided to worship me. I appreciate that. You should start by clicking on a tree stump, which will send your cultists to harvest.", 
-        "Now you have enough wood to build a town hall. Do this by clicking on the appropriate icon in the building menu, and then placing the blueprint in the world.", 
-        "Good. You should now build a mine to aquire stone. When the mine is built, it will eventually be ready to harvest. The small icon above the mine will switch from orange to green when this happens.", 
+    string[] tutorialQuestions = { "Tutorial\r\n\r\nClick on a tree stump to order your cultists to harvest wood.", 
+        "Build a town hall by clicking on the appropriate icon in the building menu, and then placing it in the world", 
+        "Now build a mine to aquire stone. The small icon above the mine will switch from orange to green when it is ready to harvest",
+        "This bar represents the happiness of your cultists. It will be reduced when your cultists do hard labor. To increase it, you must allow your cultists to relax. Build a bed and click on it to order a cultist to rest.",
         "Now that you have stone, you should build a praying stand. When you cilck on the stand, your cultists will begin a prayer session",
-        "You should notice that this bar is increased when the prayer was complete. This bar represents my appeasement with your cult, and you should always try to keep it as high as possible. It must never be reduced to zero. Press [Space] to advance", 
-        "This bar represents the happiness of your cultists. It will be reduced when your cultists do hard labor. To increase it, you must allow your cultists to relax. Build a bed and click on it to order a cultist to rest.", 
+        "You should notice that this bar is increased when the prayer was complete. This bar represents my appeasemenPress [Space] to advance", 
         "By now, you should have noticed the Task List. Everytime you order your cultists to do something, the task will appear here. If you want to prioritize a certain task, move it to the top of the list. Tasks will be prioritized based in vertical order, from highes to lowest." };
     
     string[] taskQuestions = { "Click on a tree to harvest wood, then use the wood to build a town hall", "Build a mine, then use the stone to build a prayer stand. You will also need wood" };
@@ -21,7 +22,8 @@ public class TutorialScript : MonoBehaviour
     [SerializeField] TextMeshProUGUI taskScroll;
     public GameObject tutorialBackground;
     [SerializeField] GameObject[] tutorialArrows = new GameObject[5];
-   
+    [SerializeField] GameObject tutorialButton;
+    bool tutorialIsShown = true;
 
 
     // Start is called before the first frame update
@@ -44,11 +46,18 @@ public class TutorialScript : MonoBehaviour
     {
 
         currentTutorial.text = tutorialQuestions[tutorialStage];
-        if (Input.GetKeyDown(KeyCode.Space) && tutorialStage == 4)
+        if (Input.GetKeyDown(KeyCode.Space) && tutorialStage == 5)
         {
  
             tutorialStage++;
           
+        }
+
+        if (tutorialStage > 6)
+        {
+            Destroy(tutorialBackground);
+            Destroy(currentTutorial);
+            Destroy(tutorialButton);
         }
        
 
@@ -59,6 +68,28 @@ public class TutorialScript : MonoBehaviour
         
         tutorialStage++;
         print(tutorialStage);
+
+    }
+
+    public void hideAndShowTutorial() 
+    {
+        if (tutorialIsShown == true)
+        {
+            tutorialBackground.SetActive(false);
+            currentTutorial.color = new Color(currentTutorial.color.r, currentTutorial.color.g, currentTutorial.color.b, 0);
+            tutorialStageCashe = tutorialStage;
+
+            tutorialIsShown = false;
+        }
+        else if (tutorialIsShown == false) 
+        {
+            tutorialBackground.SetActive(true);
+            currentTutorial.color = new Color(currentTutorial.color.r, currentTutorial.color.g, currentTutorial.color.b, 255);
+   
+            tutorialStage = tutorialStageCashe;
+            tutorialIsShown = true;
+
+        }
 
     }
 }
